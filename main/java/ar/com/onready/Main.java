f32f479d8aebac4b826693063f5988ee6dc5a863
Federico <aprosoff.federico@gmail.com>
2020-01-25 16:21:11 -0300

    Arrays.asList

--- main/java/ar/com/onready/Main.java
+++ main/java/ar/com/onready/Main.java
@@ -5,19 +5,21 @@ import ar.com.onready.api.Concesionaria;
 import ar.com.onready.api.Moto;
 import ar.com.onready.api.Vehiculo;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        List<Vehiculo> vehiculos = new ArrayList<Vehiculo>();
-
-        vehiculos.add(new Auto("Peugeot", "206", 4, BigDecimal.valueOf(200000)));
-        vehiculos.add(new Moto("Honda", "Titan", "125c", BigDecimal.valueOf(60000)));
-        vehiculos.add(new Auto("Peugeot", "208", 5, BigDecimal.valueOf(250000)));
-        vehiculos.add(new Moto("Yamaha", "YBR", "160c", BigDecimal.valueOf(80500.5)));
+        List<Vehiculo> vehiculos = Arrays.asList(
+                new Auto("Peugeot", "206", 4, BigDecimal.valueOf(200000)),
+                new Moto("Honda", "Titan", "125c", BigDecimal.valueOf(60000)),
+                new Auto("Peugeot", "208", 5, BigDecimal.valueOf(250000)),
+                new Moto("Yamaha", "YBR", "160c", BigDecimal.valueOf(80500.5))
+        );
 
         Concesionaria miConcesionaria = new Concesionaria(vehiculos);
 
